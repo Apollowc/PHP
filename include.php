7304de134d593b988a956f9fd3e225893f73bb5b
hpneverback <hpneverback@gmail.com>
2014-02-07 16:43:57 -0600

    update sql customerid to cid, add Session vars
    $_SESSION['cid'] = $row['cid'];
    $_SESSION['email'] = $row['email'];
    $_SESSION['chinesename'] = $row['chinesename'];
    $_SESSION['phonenumber'] = $row['phonenumber'];

--- include.php
+++ include.php
@@ -1,7 +1,7 @@
 <?php
- 	require_once 'data_valid.php';
- 	require_once 'db.php';
- 	require_once 'user_auth.php';
+// 	require_once 'data_valid.php';
+// 	require_once 'db.php';
+// 	require_once 'user_auth.php';
 	require_once 'display.php';
 // 	require_once 'url_fns.php';
 ?>
